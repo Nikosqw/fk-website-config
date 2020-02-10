9654c19bf0cef58382cb080e9f704eebc8ae0e90
Nevgup <nevgupobo@gmail.com>
2020-02-10 23:32:25 +0100

    created sidebar, added redirect links

---
+++ src/components/SideBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="sidebar" class="bg-sidebar w-2/12 h-screen">
+    <div class="flex flex-col">
+      <router-link class="py-4 pl-4 text-red-400" to="/create/teams">
+        <i class="fas fa-user-friends mr-1"></i>Equipes
+      </router-link>
+      <router-link class="py-4 pl-4 text-gray-400" to="/create/config">
+        <i class="fas fa-cog mr-1"></i>Configuration
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "sidebar"
+};
+</script>
